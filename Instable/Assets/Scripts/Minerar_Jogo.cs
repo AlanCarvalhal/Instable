7f12187b97985f290a1088c1a5e1a90266527b69
Matheus Andrade <63677287+MatheusAndrade455@users.noreply.github.com>
2021-11-18 09:37:06 -0300

    Update Minerar_Jogo.cs

--- Instable/Assets/Scripts/Minerar_Jogo.cs
+++ Instable/Assets/Scripts/Minerar_Jogo.cs
@@ -8,14 +8,12 @@ public class Minerar_Jogo : MonoBehaviour
     
     bool mouseOnOre; 
     Vector3 resize = new Vector3(5.0f, 5.0f, 5.0f);   
-    private GameObject Jogador;
-    private GameObject minerio;       
+    private GameObject Jogador;    
     public AudioSource minerando;    
 
     void Start()
     {         
-        Jogador = GameObject.FindWithTag("Player");       
-        minerio = GameObject.FindWithTag("Minerio");        
+        Jogador = GameObject.FindWithTag("Player");              
     }
 
     void Update()
@@ -23,8 +21,9 @@ public class Minerar_Jogo : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
         RaycastHit hit;       
      
-     if(Physics.Raycast(ray, out hit, 2.5f) && Input.GetKeyDown(KeyCode.F))
+     if(Physics.Raycast(ray, out hit, 2.5f) && Input.GetKeyDown(KeyCode.F) && hit.transform.gameObject.CompareTag("Minerio"))
      {
+       //https://www.monkeykidgc.com/2021/03/raycasthit-unity.html
        //transform.localScale = transform.localScale - resize; (consertar pra dar resize no objeto olhado)
        //adicionar no inventario dependendo qual for
         minerando.Play(); 
